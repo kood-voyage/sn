0c3da0fe9c04b1382840bda4556aaa640deea9c3
mannuk <annukmarkusoliver@gmail.com>
2024-02-25 23:50:34 +0200

    commit fixing terraform infra plan and apply stages with credentials/mannuk

--- infra/providers.tf
+++ infra/providers.tf
@@ -19,6 +19,6 @@ terraform {
 }
 provider "aws" {
     region                   = "us-east-1"
-    shared_credentials_files = ["~/.aws/credentials"]
+    
     profile                  = "vscode"
   }
