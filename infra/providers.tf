35322351978e0359a2f3925fdc63e774bf378307
mannuk <annukmarkusoliver@gmail.com>
2024-02-25 23:58:26 +0200

    commit fixing terraform infra plan and apply stages with credentials/mannuk

--- infra/providers.tf
+++ infra/providers.tf
@@ -18,7 +18,8 @@ terraform {
   
 }
 provider "aws" {
-    region                   = "us-east-1"
-    shared_credentials_files = ["/github/home/.aws/credentials"]
+    region                  = "us-east-1"
+    access_key              = var.aws_access_key_id
+    secret_key              = var.aws_secret_access_key
     profile                  = "vscode"
   }
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,11 @@ variable "host_os" {
      type = string
      default = "mac"
 }
+
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+}
