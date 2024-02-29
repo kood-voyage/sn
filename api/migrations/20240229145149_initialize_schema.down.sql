39fadc5f13c8f5846a96130ab5868487c6916d68
Nikita Baranov <kalima96@mail.ru>
2024-02-29 19:20:51 +0200

    db migrations

---
+++ api/migrations/20240229145149_initialize_schema.down.sql
@@ -0,0 +1,43 @@
+DROP TABLE user;
+
+DROP TABLE follower;
+
+DROP TABLE post;
+
+DROP TABLE comment;
+
+DROP TABLE event;
+
+DROP TABLE community;
+
+DROP TABLE event_option;
+
+DROP TABLE selected_event_option;;
+
+DROP TABLE chat;
+
+DROP TABLE chat_users;
+
+DROP TABLE chat_lines;
+
+DROP TABLE request;
+
+DROP TABLE request_type;
+
+DROP TABLE privacy;
+
+DROP TABLE privacy_type;
+
+DROP TABLE reaction;
+
+DROP TABLE reaction_join;
+
+DROP TABLE member;
+
+DROP TABLE member_type;
+
+DROP TABLE image;
+
+DROP TABLE image_type;;
+
+DROP TABLE session;
