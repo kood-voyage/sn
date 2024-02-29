ff161ce9b2e97e88e4b215612e544db62bea1fc9
hmahar <harrimahar@gmail.com>
2024-02-29 02:15:05 +0200

    SN-2 Removed unneccessary testing functions from main.go

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"social-network/internal/app/server"
-	"social-network/pkg/validator"
 )
 
 func main() {
@@ -14,23 +12,5 @@ func main() {
 		log.Fatalf("Error reading config file: %s\n", err)
 	}
 
-	test()
 	log.Fatal(server.Start(config))
-}
-
-func test() {
-	type user struct {
-		Email    string `json:"email" validate:"required|min_len:5|max_len:25|email"`
-		Username string `json:"username" validate:"min_len:5|integer"`
-		Age      int    `json:"age"`
-		Testfield []string `validate:"min_len:1"`
-	}
-
-	user1 := user{
-		Email:    "johndoe@gmail.com",
-		Username: "22222",
-		Age:      2,
-		Testfield: []string{"hello", "world"},
-	}
-	fmt.Println(validator.Validate(user1))
 }
