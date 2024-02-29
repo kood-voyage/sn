19a6bae33f6f4ac6699638e4660abac9a28ad1f2
hmahar <harrimahar@gmail.com>
2024-02-29 02:09:36 +0200

    SN-2 Added testings for validator

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -23,12 +23,14 @@ func test() {
 		Email    string `json:"email" validate:"required|min_len:5|max_len:25|email"`
 		Username string `json:"username" validate:"min_len:5|integer"`
 		Age      int    `json:"age"`
+		Testfield []string `validate:"min_len:1"`
 	}
 
 	user1 := user{
 		Email:    "johndoe@gmail.com",
 		Username: "22222",
 		Age:      2,
+		Testfield: []string{"hello", "world"},
 	}
 	fmt.Println(validator.Validate(user1))
 }
