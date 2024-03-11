9c538fa815888af8a8c4890dab5760f25b88c09f
Markus Oliver Annuk <149253918+markusoliverannuk@users.noreply.github.com>
2024-03-11 22:55:22 +0200

    infra subnet modifications

--- infra/main.tf
+++ infra/main.tf
@@ -65,6 +65,18 @@ resource "aws_subnet" "dev_public_subnet" {
   }
 }
 
+resource "aws_subnet" "dev_public_subnet" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+
+  tags = {
+    Name = "dev-public-subnet"
+  }
+}
+
 //dont modify the following, guys
 
 resource "aws_internet_gateway" "dev_internet_gateway" {
