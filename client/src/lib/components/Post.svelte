5cc821b48e992bde11ee719394463d9f8c307c2c
Zindiks <103096207+Zindiks@users.noreply.github.com>
2024-03-13 16:03:49 +0200

    POST card + post review

--- client/src/lib/components/Post.svelte
+++ client/src/lib/components/Post.svelte
@@ -3,28 +3,66 @@
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
+
+	import { type CarouselAPI } from '$lib/components/ui/carousel/context.js';
+	import * as Carousel from '$lib/components/ui/carousel/index.js';
+
+	let api: CarouselAPI;
+	let count = 0;
+	let current = 0;
+
+	$: if (api) {
+		count = api.scrollSnapList().length;
+		current = api.selectedScrollSnap() + 1;
+		api.on('select', () => {
+			current = api.selectedScrollSnap() + 1;
+		});
+	}
+
+	export let article: any;
 </script>
 
 <Dialog.Root>
-	<Dialog.Trigger class="bg-red-500 h-96 w-full sm:rounded-lg mb-1 sm:mb-0 ">
+	<Dialog.Trigger class="h-96 w-full sm:rounded-lg mb-1 sm:mb-0 ">
 		<div class="p-1">
-			<img
-				src="https://images.twinkl.co.uk/tw1n/image/private/t_630/u/ux/twinkl-video-games-2_ver_1.jpg"
-			/>
-			<p>Game industry standarts</p>
-			<p class="lines3 text-sm">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lobortis erat nec massa
-				convallis scelerisque. Suspendisse sed accumsan neque. Sed malesuada, velit nec
-				venenatisdasdasdawq
-			</p>
+			<img src={article.imgURL} alt="image1" />
+			<p class="text-lg text-ellipsis w-full text-left">{article.title}</p>
+			<p class="lines3 text-sm text-left text-slate-400">{article.body}</p>
 		</div>
 	</Dialog.Trigger>
-	<Dialog.Content class="w-full">
-		<Dialog.Header>
-			<Dialog.Title>Edit profile</Dialog.Title>
+
+	<Dialog.Content>
+		<Dialog.Header class="">
+			<Dialog.Title>{article.title}</Dialog.Title>
+
 			<Dialog.Description>
-				Make changes to your profile here. Click save when you're done.
+				{article.body}
 			</Dialog.Description>
+
+			<Carousel.Root bind:api>
+				<Carousel.Content>
+					<Carousel.Item><img src={article.imgURL} alt="image1" /></Carousel.Item>
+					<Carousel.Item
+						><img
+							src="https://d1k8pxxip4mxx2.cloudfront.net/pub/media/bl/20201102-10090/large.jpg?qySIduQjDI9fuqUQexe97w=="
+							alt="image1"
+						/></Carousel.Item
+					>
+					<Carousel.Item
+						><img
+							src="https://resources.finalsite.net/images/v1629478453/usmk12org/yizzw0mr1escg58pmhvp/esports.png"
+							alt="image1"
+						/></Carousel.Item
+					>
+				</Carousel.Content>
+
+				<Carousel.Previous class="left-2" />
+				<Carousel.Next class="right-2" />
+			</Carousel.Root>
+
+			<div class="py-2 text-center text-sm text-muted-foreground">
+				Slide {current} of {count}
+			</div>
 		</Dialog.Header>
 		<div class="w-full h-full"></div>
 	</Dialog.Content>
--- client/src/routes/(app)/app/u/[username]/+page.svelte
+++ client/src/routes/(app)/app/u/[username]/+page.svelte
@@ -16,6 +16,30 @@
 		followers: 1,
 		following: 0
 	};
+
+	const demoData = [
+		{
+			imgURL: 'https://c.files.bbci.co.uk/E909/production/_112375695_crucible976.jpg',
+			title: 'The Evolution of Gaming Consoles',
+			body: 'The evolution of gaming consoles has been a fascinating journey, marked by technological advancements, innovative design, and fierce competition among industry giants. From the humble beginnings of the Magnavox Odyssey to the groundbreaking release of the PlayStation 5 and Xbox Series X, gaming consoles have continuously pushed the boundaries of gaming experiences. This article explores the key milestones, breakthroughs, and cultural impact of gaming consoles throughout history.'
+		},
+		{
+			imgURL:
+				'https://assetsio.reedpopcdn.com/best-fps-header.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp',
+			title: 'Top 10 Must-Play Indie Games of the Year',
+			body: 'Indie games continue to captivate players with their unique art styles, compelling narratives, and innovative gameplay mechanics. In this article, we showcase the top 10 must-play indie games of the year, spanning various genres and platforms. From emotionally charged adventures to mind-bending puzzles, these indie gems offer unforgettable experiences that rival even the biggest AAA titles. Get ready to discover your next gaming obsession!'
+		},
+		{
+			imgURL: 'https://www.premiumbeat.com/blog/wp-content/uploads/2022/08/Best-VR-Film.jpg',
+			title: 'Exploring the World of Virtual Reality Gaming',
+			body: 'Virtual reality gaming has emerged as one of the most immersive and exhilarating forms of entertainment, transporting players to fantastical worlds and pushing the boundaries of reality. In this article, we delve into the exciting world of virtual reality gaming, exploring the latest hardware advancements, groundbreaking titles, and the transformative impact of VR technology on the gaming industry. Strap on your headset and get ready to experience gaming like never before!'
+		},
+		{
+			imgURL: 'https://dossiers.dhnet.be/esport/media/08583030-photo-esport-mr.jpg',
+			title: 'The Rise of Esports: Competitive Gaming Takes the World by Storm',
+			body: "Esports has evolved from niche hobby to global phenomenon, captivating millions of viewers and offering lucrative opportunities for skilled players around the world. In this article, we examine the meteoric rise of esports, tracing its humble beginnings in local arcades to sold-out arenas and multi-million dollar tournaments. From the strategic depth of MOBAs to the lightning-fast reflexes of first-person shooters, esports encompasses a diverse array of competitive gaming experiences that continue to push the boundaries of what's possible in gaming."
+		}
+	];
 </script>
 
 <svelte:head>
@@ -32,7 +56,7 @@
 			<div class="m-auto h-24 sm:h-40 max-w-[1096px] p-0 sm:p-4">
 				<img
 					class="h-full w-full rounded-none sm:rounded-xl object-cover"
-					src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+					src="https://upload.wikimedia.org/wikipedia/commons/2/28/Iskandar_Investment_Esports_Carnival_2019.jpg"
 					alt="banner"
 				/>
 			</div>
@@ -101,22 +125,9 @@
 		<!-- profile activity / posts -->
 
 		<div class="h-full w-full sm:grid sm:grid-cols-2 md:grid-cols-3 gap-1 p-0 sm:p-4 mt-5 md:mt-0">
-			<Post />
-			<Post />
-
-			<div class="bg-red-500 h-96 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
-			<div class="bg-yellow-500 h-56 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
-			<div class="bg-orange-500 h-56 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
-			<div class="bg-pink-500 h-56 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
-			<div class="bg-purple-500 h-56 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
-			<div class="bg-red-500 h-56 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
-			<div class="bg-yellow-500 h-56 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
-			<div class="bg-orange-500 h-56 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
-			<div class="bg-pink-500 h-56 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
-			<div class="bg-purple-500 h-56 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
-			<div class="bg-red-500 h-56 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
-			<div class="bg-yellow-500 h-56 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
-			<div class="bg-orange-500 h-56 w-full sm:rounded-lg mb-1 sm:mb-0">123</div>
+			{#each demoData as data}
+				<Post article={data} />
+			{/each}
 		</div>
 	</div>
 </main>
