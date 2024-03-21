dcd56f75e4a849105f72948903e469f30f0a9ec5
hmahar <harrimahar@gmail.com>
2024-03-21 12:14:26 +0200

    SN-23 testings

--- api/internal/app/server/event.go
+++ api/internal/app/server/event.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 	"social-network/internal/model"
+	"social-network/pkg/validator"
 )
 
 func (s *Server) createEvent() http.HandlerFunc {
@@ -16,13 +17,29 @@ func (s *Server) createEvent() http.HandlerFunc {
 		}
 
 		if err := s.decode(r, event); err != nil {
-			s.error(w, http.StatusInternalServerError, err)
+			s.error(w, http.StatusUnprocessableEntity, err)
 			return
 		}
 		event.UserID = userID
 
+		if err := validator.Validate(event); err != nil {
+			s.error(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		//check if creator is part of a group
+		t, err := s.store.Group().IsMember(event.GroupID, userID)
+		if err != nil {
+			s.error(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+		if !t {
+			s.error(w, http.StatusUnprocessableEntity, errors.New("user is not part of the group"))
+			return
+		}
+
 		if err := s.store.Event().Create(event); err != nil {
-			s.error(w, http.StatusInternalServerError, err)
+			s.error(w, http.StatusUnprocessableEntity, err)
 			return
 		}
 
@@ -38,8 +55,32 @@ func (s *Server) updateEvent() http.HandlerFunc {
 
 func (s *Server) deleteEvent() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		if err := s.store.Event().Delete(id); err != nil {
+		eventId := r.PathValue("id")
+
+		userID, ok := r.Context().Value(ctxUserID).(string)
+		if !ok {
+			s.error(w, http.StatusUnauthorized, errors.New("unauthorized"))
+			return
+		}
+		//if event exists and get groupID
+		event, err := s.store.Event().Get(eventId)
+		if err != nil {
+			s.error(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		//check if user is part of a group
+		t, err := s.store.Group().IsMember(event.GroupID, userID)
+		if err != nil {
+			s.error(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+		if !t {
+			s.error(w, http.StatusUnprocessableEntity, errors.New("user is not part of the group to delete an event"))
+			return
+		}
+
+		if err := s.store.Event().Delete(eventId); err != nil {
 			s.error(w, http.StatusInternalServerError, err)
 			return
 		}
--- api/internal/model/event.go
+++ api/internal/model/event.go
@@ -1,16 +1,17 @@
 package model
 
 import (
-	"github.com/google/uuid"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type Event struct {
-	ID          string    `db:"id" json:"id"`
-	UserID      string    `db:"user_id" json:"user_id"`
-	GroupID     string    `db:"group_id" json:"group_id"`
-	Name        string    `db:"name" json:"name"`
-	Description string    `db:"description" json:"description"`
+	ID          string    `db:"id" json:"id" validate:"required"`
+	UserID      string    `db:"user_id" json:"user_id" validate:"required"`
+	GroupID     string    `db:"group_id" json:"group_id" validate:"required"`
+	Name        string    `db:"name" json:"name" validate:"required|min_len:4|max_len:25"`
+	Description string    `db:"description" json:"description" validate:"required|min_len:4|max_len:25"`
 	CreatedAt   time.Time `db:"created_at" json:"created_at"`
 	Date        time.Time `db:"date" json:"date"`
 }
